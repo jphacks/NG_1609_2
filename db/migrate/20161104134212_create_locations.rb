ad820d55387229c9bf97b3197b325e7f0f7bead6
GuiltyMorishita <morishita@lang.cs.tut.ac.jp>
2016-11-05 11:25:02 +0900

    locationテーブルとデータを作成

--- db/migrate/20161104134212_create_locations.rb
+++ db/migrate/20161104134212_create_locations.rb
@@ -1,6 +1,13 @@
 class CreateLocations < ActiveRecord::Migration[5.0]
   def change
     create_table :locations do |t|
+      t.string 'name', null: false
+      t.decimal 'latitude', precision: 9, scale: 6, null: false
+      t.decimal 'longitude', precision: 9, scale: 6, null: false
+      t.text 'description', null: false
+      t.string 'imageUrl', null: false, default: ''
+      t.string 'kind', null: false
+      t.float 'requiredTime', null: false
 
       t.timestamps
     end
