666c89307f75f36dc0e7d8f32e13ff90758c04f4
GuiltyMorishita <morishita@lang.cs.tut.ac.jp>
2016-11-05 19:35:23 +0900

    地域ごとのlocationリストを取得できるようにした

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -1,6 +1,6 @@
 module Api
   class LocationsController < ApplicationController
-    before_action :set_location, only: [:show, :update, :destroy]
+    before_action :set_location, only: [:update, :destroy]
 
     # GET /locations
     def index
@@ -9,9 +9,11 @@ module Api
       render json: @locations
     end
 
-    # GET /locations/1
-    def show
-      render json: @location
+    # GET /region_locations
+    def region_locations
+      @locations = Location.where(region_id: params[:region_id])
+
+      render json: @locations
     end
 
     # POST /locations
